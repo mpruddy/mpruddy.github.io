c53822d28c87577a7d1c9a4b0a7ccf559312febe
mpruddy <mpruddy@gmail.com>
2022-12-06 20:01:50 -0600

    again put website into github

---
+++ docs/assets/documents/Website/_vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|MAIN\\DADDY
+vti_modifiedby:SR|MAIN\\DADDY
+vti_timelastmodified:TR|05 Jun 2005 03:34:57 -0000
+vti_timecreated:TR|05 Jun 2005 03:34:57 -0000
+vti_cacheddtm:TX|05 Jun 2005 03:34:57 -0000
+vti_filesize:IR|16355
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|
